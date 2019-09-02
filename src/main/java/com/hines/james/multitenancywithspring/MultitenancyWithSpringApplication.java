b51c002a5cccda32f5363317129dd2d6a32782d8
James Hines <jameshines10@gmail.com>
2019-09-02 17:00:55 -0500

    Initial commit

---
+++ src/main/java/com/hines/james/multitenancywithspring/MultitenancyWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hines.james.multitenancywithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenancyWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenancyWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hines/james/multitenancywithspring/MultitenancyWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hines.james.multitenancywithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenancyWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
