a8e75ddd2d703fb439657cdb1122f753f6501c7d
James Hines <jameshines10@gmail.com>
2019-09-02 17:23:41 -0500

    Adds an entity class and configuration for one datasource

---
+++ src/main/java/com/hines/james/multitenancywithspring/entities/User.java
@@ -0,0 +1,22 @@
+package com.hines.james.multitenancywithspring.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Data
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+}
