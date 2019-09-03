fc6490f89fccfc4c1891447b9b1f8b5200bc673d
James Hines <jameshines10@gmail.com>
2019-09-02 23:42:11 -0500

    Dont need headers anymore

--- src/main/java/com/hines/james/multitenancywithspring/controllers/MainController.java
+++ src/main/java/com/hines/james/multitenancywithspring/controllers/MainController.java
@@ -21,7 +21,7 @@ public class MainController {
     private UserRepository userRepository;
 
     @PostMapping(path="/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email, @RequestHeader HttpHeaders headers) {
+    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
         User n = new User();
